fcc779201a8b18610c1f897552fa352dabaf1ed0
Yanis Falaki <yanisfalaki@outlook.com>
2024-05-20 06:38:09 -0400

    mlp_pid with more data

--- generate_pid_dataset.py
+++ generate_pid_dataset.py
@@ -6,10 +6,9 @@ import pandas as pd
 pid = PIDController()
 env = Environment()
 state = env.reset()
-ep_reward = 0
 data = []
 
-for i in range(2500): # 20k different csv files
+for i in range(5002, 10000): # 20k different csv files
     pid = PIDController()
     env = Environment(data_path=f'./data/{str(i).zfill(5)}.csv')
     state = env.reset()
@@ -35,4 +34,4 @@ for i in range(2500): # 20k different csv files
 print(len(data))
 df = pd.DataFrame(data, columns=["target_lataccel", "current_lataccel", "vEgo", "aEgo", "roll", "error", "prev_error", "integral", "derivative", "prev_derivative", "prev_action", "steerCommand"])
 
-df.to_csv('pid_controller_data.csv', index=False)
+df.to_csv('pid_controller_data.csv', index=False, mode='a', header=False)
